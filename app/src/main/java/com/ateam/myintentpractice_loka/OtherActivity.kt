46ea0f1ce3d3955801b6d070b9037f33bf8c5f88
Loka017 <jessinthespace@gmail.com>
2021-09-14 06:13:57 +0900

    메세지를 보내는화면과/받는화면 추가

--- app/src/main/java/com/ateam/myintentpractice_loka/OtherActivity.kt
+++ app/src/main/java/com/ateam/myintentpractice_loka/OtherActivity.kt
@@ -1,11 +1,21 @@
 package com.ateam.myintentpractice_loka
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        returnToMainBtn.setOnClickListener {
+
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+            finish()//지금화면을 종료
+        }
     }
 }
