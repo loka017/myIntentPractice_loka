371657f18172d23c6b928165ecd2bab513cbb9f8
Loka017 <jessinthespace@gmail.com>
2021-09-14 06:46:37 +0900

    닉네임 입력용 별개 화면 추가

---
+++ app/src/main/java/com/ateam/myintentpractice_loka/EditNicknameActivity2.kt
@@ -0,0 +1,11 @@
+package com.ateam.myintentpractice_loka
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname2)
+    }
+}
